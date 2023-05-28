85f881efcef6033db0c4f4816cc620086ac7d55b
Cardano APEX Stake Pool <79419090+cardano-apexpool@users.noreply.github.com>
2023-05-28 13:21:40 +0200

    Formatting

--- vesting_oracle_fee/oracle/contract.py
+++ vesting_oracle_fee/oracle/contract.py
@@ -3,6 +3,13 @@ from opshin.ledger.interval import *
 
 @dataclass
 class PublishParams(PlutusData):
+    """
+    Datum format
+    owner: publisher of the datum, who can also reclaim the UTxO after the deadline (refund)
+    deadline:  the deadline after which the datum UTxO can be refunded
+    info: the useful information in the oracle datum, an int in this case
+    """
+
     owner: PubKeyHash
     deadline: POSIXTime
     info: int
@@ -14,10 +21,9 @@ class RefundRedeemer(PlutusData):
 
 
 def validator(
-        datum: PublishParams,
-        redeemer: RefundRedeemer,
-        context: ScriptContext
+    datum: PublishParams, redeemer: RefundRedeemer, context: ScriptContext
 ) -> None:
-
-    assert contains(make_from(datum.deadline), context.tx_info.valid_range), "TX submitted too early!"
+    assert contains(
+        make_from(datum.deadline), context.tx_info.valid_range
+    ), "TX submitted too early!"
     assert datum.owner in context.tx_info.signatories, "Refund signature missing!"
